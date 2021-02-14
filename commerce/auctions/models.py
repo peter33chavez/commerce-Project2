57da7d1ae3ade5f164d4b8432529193ef6181904
Peter Chavez <peter33chavez@gmail.com>
2021-02-13 17:36:13 -0800

    added 3 models auctionListings, listingsBids, listingsComments

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -10,16 +10,21 @@ class auctionListings(models.Model):
     title = models.CharField(max_length=80)
     description = models.CharField(max_length=4000)
     startingPrice = models.DecimalField(max_digits=10000, decimal_places=2)
-    imgUrl = models.URLField(max_length=200, null=True)
+    imgUrl = models.URLField(max_length=200, blank=True)
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return "$" + str(self.startingPrice)
     
-class listingBids(models.Model):
-    listingID = models.ForeignKey(related_name=)
+
+class listingsBids(models.Model):
+    listingID = models.ForeignKey(auctionListings, on_delete=models.CASCADE)
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+    bids = models.DecimalField(max_digits=10000, decimal_places=2)
     
 
-
-class listingComments(models.Model):
-    pass
+class listingsComments(models.Model):
+    listingID = models.ForeignKey(auctionListings, on_delete=models.CASCADE)
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = models.CharField(max_length=80)
 #TODO
