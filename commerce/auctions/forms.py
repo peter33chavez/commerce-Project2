ca8ac75d9b0ffa06a35ed0fff42661d178dc3378
Peter Chavez <peter33chavez@gmail.com>
2021-04-17 19:29:54 -0700

    adding styping  to the creat-listing page

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -4,8 +4,12 @@ from .models import auctionListings
 class listingForm(forms.ModelForm):
     class Meta:
         model = auctionListings
+        widgets = {
+            'description': forms.TextInput(attrs={'class': 'new-listing-description'})
+        }
         fields = ('title','description','startingPrice','imgUrl', 'category')
         labels = { 
             'imgUrl': 'Image Url',
             'startingPrice': 'Starting Price' 
          }
+
