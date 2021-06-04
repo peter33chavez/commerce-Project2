8996cc9c21af3aed70be25cdbbfe0e113956f4a3
Peter Chavez <peter33chavez@gmail.com>
2021-06-03 19:09:04 -0700

    moving

--- commerce/auctions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-
-admin.site.register(auctionListings)
-admin.site.register(listingsBids)
-admin.site.register(listingsComments)
-admin.site.register(categories)
