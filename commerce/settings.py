a163e59b3f800a1f41b1f62e21a10343f1073e72
Peter Chavez <peter33chavez@gmail.com>
2021-06-20 12:31:51 -0700

    test

--- commerce/settings.py
+++ commerce/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticmain')
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
